de32756a0a56af8166990d22f054510d47a14560
tazimete <tazimete@gmail.com>
2021-07-23 17:35:31 +0600

    Solved gitignore issue

--- tawktestios/Pods/Target Support Files/Pods-tawktestios-tawktestiosUITests/Pods-tawktestios-tawktestiosUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_tawktestios_tawktestiosUITests : NSObject
-@end
-@implementation PodsDummy_Pods_tawktestios_tawktestiosUITests
-@end
--- tawktestios/Pods/Target Support Files/Pods-tawktestios/Pods-tawktestios-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_tawktestios : NSObject
-@end
-@implementation PodsDummy_Pods_tawktestios
-@end
