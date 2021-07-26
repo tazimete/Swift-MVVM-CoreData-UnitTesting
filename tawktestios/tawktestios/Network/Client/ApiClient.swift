6dec4cb0d77dc4405e8821dd140b73fb50c156d6
tazimete <tazimete@gmail.com>
2021-07-26 18:37:08 +0600

    Replacing Observable with completionHqndler

--- tawktestios/tawktestios/Network/Client/ApiClient.swift
+++ tawktestios/tawktestios/Network/Client/ApiClient.swift
@@ -9,6 +9,16 @@ import Foundation
 import RxSwift
 import RxCocoa
 
+class ApiClientError: Error {
+    let code:Int
+    let message: String
+    
+    init(code: Int, message: String) {
+        self.code = code
+        self.message = message
+    }
+}
+
 class APIClient {
     public static let shared = APIClient()
     private let queueManager: QueueManager
@@ -20,6 +30,49 @@ class APIClient {
             .map { data in
                 try JSONDecoder().decode(T.self, from: data)
             }.observe(on: MainScheduler.asyncInstance)
+    }
+    
+    func send<T: Codable>(apiRequest: APIRequest, type: T.Type, completeionHandler: @escaping (_ response: T?, _ error: Error?) -> Void){
+        let session = URLSession.shared
+        let request = apiRequest.request(with: apiRequest.baseURL)
+           
+//       var request = URLRequest(url: url)
+//       request.httpMethod = "GET"
+//       request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        let task = session.dataTask(with: request) { data, response, error in
+
+           if error != nil || data == nil {
+               print("Client error!")
+               return
+           }
+
+           guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
+               print("Server error!")
+               return
+           }
+
+           guard let mime = response.mimeType, mime == "application/json" else {
+               print("Wrong MIME type!")
+               return
+           }
+
+           guard let responseData = data else{
+               print("No data")
+               return
+           }
+            
+            let resultjson = try? JSONDecoder().decode(T.self, from: responseData)
+           
+            if let result = resultjson{
+                completeionHandler(result, nil)
+            }else{
+                let error = ApiClientError(code: 100, message: "Error response")
+                completeionHandler(nil, error)
+            }
+       }
+
+       task.resume()
     }
     
     init(withQueueManager queueManager: QueueManager = QueueManager.shared) {
