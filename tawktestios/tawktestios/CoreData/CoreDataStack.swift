beadaae69c0a168a2222de867385479c0fae48a5
tazimete <tazimete@gmail.com>
2021-07-30 00:52:04 +0600

    Implementing core data for data persistance

---
+++ tawktestios/tawktestios/CoreData/CoreDataStack.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataStack.swift
+//  tawktestios
+//
+//  Created by JMC on 29/7/21.
+//
+
+import Foundation
---
+++ tawktestios/tawktestios/CoreData/Film.swift
@@ -0,0 +1,8 @@
+//
+//  Film.swift
+//  tawktestios
+//
+//  Created by JMC on 29/7/21.
+//
+
+import Foundation
