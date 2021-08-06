510ea6d028511f3af39f0758510cde61f312603c
tazimete <tazimete@gmail.com>
2021-08-07 02:49:34 +0600

    Added inverted symbol

--- tawktestios/tawktestios/View/Github/Cell/GithubUserCellInverted.swift
+++ tawktestios/tawktestios/View/Github/Cell/GithubUserCellInverted.swift
@@ -57,6 +57,16 @@ class GithubUserCellInverted: UITableViewCell, ConfigurableCell {
         return lbl
     }()
     
+    private let lblType : UILabel = {
+        let lbl = UILabel()
+        lbl.textColor = .black
+        lbl.font = UIFont.systemFont(ofSize: 12)
+        lbl.textAlignment = .left
+        lbl.numberOfLines = 0
+        lbl.text = "Inverted"
+        return lbl
+    }()
+    
     private let ivAvatar : UIImageView = {
         let imgView = UIImageView(image: UIImage(named: "img_avatar"))
         imgView.contentMode = .scaleAspectFit
@@ -81,11 +91,13 @@ class GithubUserCellInverted: UITableViewCell, ConfigurableCell {
         containerView.addSubview(ivAvatar)
         containerView.addSubview(lblUsername)
         containerView.addSubview(lblDescription)
+        containerView.addSubview(lblType)
         
         containerView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 5, paddingLeft: 5, paddingBottom: 5, paddingRight: 5, width: frame.width, height: 0, enableInsets: false)
         ivAvatar.anchor(top: containerView.topAnchor, left:  containerView.leftAnchor, bottom: containerView.bottomAnchor, right: nil, paddingTop: 5, paddingLeft: 5, paddingBottom: 5, paddingRight: 0, width: 90, height: 90, enableInsets: false)
         lblUsername.anchor(top: containerView.topAnchor, left: ivAvatar.rightAnchor, bottom: nil, right: containerView.rightAnchor, paddingTop: 20, paddingLeft: 10, paddingBottom: 0, paddingRight: 15, width: frame.size.width, height: 0, enableInsets: false)
         lblDescription.anchor(top: lblUsername.bottomAnchor, left: ivAvatar.rightAnchor, bottom: containerView.bottomAnchor, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 10, paddingBottom: 15, paddingRight: 15, width: frame.size.width, height: 0, enableInsets: false)
+        lblType.anchor(top: containerView.topAnchor, left: lblUsername.rightAnchor, bottom: nil, right: containerView.rightAnchor, paddingTop: 10, paddingLeft: 10, paddingBottom: 10, paddingRight: 10, width: 50, height: 0, enableInsets: false)
     }
     
     required init?(coder aDecoder: NSCoder) {
